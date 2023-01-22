b798f3b5a4f62fec666e71ff614b4e55e8769305
Ashraf Uddin <ashrafpeal@gmail.com>
2023-01-22 19:27:47 +0600

    Increment & Dicriment testing done. Besic of state management with vanilla js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-'use scrict';
+// 'use scrict';
+
+// select Element
+const counter = document.getElementById( 'counter' );
+const increment = document.getElementById( 'increment' );
+const decrement = document.getElementById( 'decrement' );
+const message = document.getElementById( 'message' );
+
+// initializing
+let count = 0;
+
+increment.addEventListener( 'click', () => {
+    count++;
+    counter.innerHTML = count;
+    // console.log( count );
+});
+
+decrement.addEventListener( 'click', () => {
+    count--;
+    // if (-1 == count) {
+    //     message.innerHTML = "You have reached the limit.";
+    // }
+    counter.innerHTML = count;
+    // console.log( count );
+});
